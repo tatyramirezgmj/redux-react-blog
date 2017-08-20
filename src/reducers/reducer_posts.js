b03ba36f9d136fdf82046ba0349f58130f6709da
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-08-20 19:35:06 -0400

    Updates on reducers

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,7 +4,7 @@ import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case DELETE_POST:
-    return _.omit(state, action.payload);  
+    return _.omit(state, action.payload);
 
     case FETCH_POST:
     return{...state, [action.payload.data.id]: action.payload.data };
