e0bbcd8abfa0dd70f2726e6f27e6f99c86a64397
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-08-20 19:24:45 -0400

    Deleting a post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -6,14 +6,18 @@ import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount(){
+    if(!this.props.post){
     this.props.fetchPosts();
+    }
   }
 
   renderPosts(){
   return  _.map(this.props.posts, post => {
     return (
       <li className="list-group-item" key={post.id}>
+        <Link to={`/posts/${post.id}`}>
         {post.title}
+        </Link>
       </li>
     )
   })
